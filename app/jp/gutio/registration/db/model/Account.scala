b5d4aac191c4aacad2a817431592c4c3de39cbc5
Yamaguchi Daisuke <gutio81@gmail.com>
2018-01-09 15:42:04 +0900

    ScalikeJDBC用モデルクラス

--- app/jp/gutio/registration/db/model/Account.scala
+++ app/jp/gutio/registration/db/model/Account.scala
@@ -1,5 +1,29 @@
 package jp.gutio.registration.db.model
 
-class Account {
+import java.time._
+import scalikejdbc._
 
+case class Account (
+  id : Long,
+  accessToken : String,
+  sessionKey : String,
+  createdAt : ZonedDateTime,
+  updatedAt : ZonedDateTime
+)
+
+object Account extends SQLSyntaxSupport[Account]{
+
+  override val tableName = "Account"
+  override def columns: Seq[String] = Seq("id","accessToken","sessionKey","createdAt","updatedAt")
+  override def useSnakeCaseColumnName: Boolean = false
+
+  def apply(g: ResultName[Account])(rs: WrappedResultSet) : Account = {
+    Account(
+      id = rs.long(g.id),
+      accessToken = rs.string(g.accessToken),
+      sessionKey = rs.string(g.sessionKey),
+      createdAt = rs.zonedDateTime(g.createdAt),
+      updatedAt = rs.zonedDateTime(g.updatedAt)
+    )
+  }
 }
