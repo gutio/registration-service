c6b6b995a5ed7877f76926d8552fac6f8c080574
Yamaguchi Daisuke <gutio81@gmail.com>
2018-01-11 13:02:15 +0900

    json4sへ乗り換え
    circeでJSON由来クラスを継承を用いて設計すると、現状メリットがあまりないためjson4sでコーデックする

--- app/controllers/SettingController.scala
+++ app/controllers/SettingController.scala
@@ -3,11 +3,11 @@ package controllers
 import javax.inject._
 
 import play.api.mvc._
+import com.github.tototoshi.play2.json4s.Json4s
+import org.json4s._
 
-import io.circe.generic.auto._
-import io.circe.syntax._
-
-import jp.gutio.registration.externals.{GoogleIdVerifier}
+import jp.gutio.registration.externals.GoogleIdVerifier
+import jp.gutio.registration.settings.GoogleIdVerifierSetting
 
 /**
   * 設定情報レスポンス用コントローラ
@@ -15,9 +15,13 @@ import jp.gutio.registration.externals.{GoogleIdVerifier}
   * Created by Daisuke Yamaguchi on 2017/12/25.
   */
 @Singleton
-class SettingController @Inject()(cc: ControllerComponents) extends BaseController(cc) {
+class SettingController @Inject()(cc: ControllerComponents, json4s: Json4s) extends AbstractController(cc) {
+
+  import json4s.implicits._
+  implicit val formats = DefaultFormats
 
   def getGoogleSetting() = Action { implicit request: Request[AnyContent] =>
-    Ok(GoogleIdVerifier.settingOpt.asJson.noSpaces)
+    val setting = GoogleIdVerifier.settingOpt.getOrElse(GoogleIdVerifierSetting.empty)
+    Ok(Extraction.decompose(setting))
   }
 }
