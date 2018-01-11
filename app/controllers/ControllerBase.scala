878aa9e85daaf7afb4e94471532feeee7992ff23
Yamaguchi Daisuke <gutio81@gmail.com>
2018-01-11 13:18:53 +0900

    そのままは使わない基底クラスのネーミングルール合わせ

--- app/controllers/ControllerBase.scala
+++ app/controllers/ControllerBase.scala
@@ -6,6 +6,6 @@ import play.api.mvc.{AbstractController, ControllerComponents}
   * コントローラの共通ロジック
   * Created by Daisuke Yamaguchi on 2017/12/25.
   */
-class BaseController(cc: ControllerComponents) extends AbstractController(cc) {
+abstract class ControllerBase(cc: ControllerComponents) extends AbstractController(cc) {
 
 }
